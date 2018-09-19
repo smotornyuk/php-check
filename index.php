92f01db63a2b3761bbfaa7a7ed0c396fec15efe0
Sergey Motornyuk <xxx@yyy.com>
2018-09-19 20:13:08 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Document</title>
+    </head>
+    <body>
+	<h1>hello world</h1>
+<?php echo '<h2>bye world</h2>'; ?>
+    </body>
+</html>
